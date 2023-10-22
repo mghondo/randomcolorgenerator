470d1f05d857bc9621efab798cad52502768d9d8
Morgan Hondros <morganhondros@Morgans-MacBook-Pro.local>
2023-10-22 13:08:28 -0400

    lotta copying functionality added.

--- my-react-app/src/randomcolor.jsx
+++ my-react-app/src/randomcolor.jsx
@@ -7,6 +7,7 @@ class ColorSchemeGenerator extends Component {
     this.state = {
       colors: Array(6).fill(null).map(() => this.generateRandomColor()),
       lockedColors: Array(6).fill(false),
+      showColorList: false, // Added state for showing/hiding the color list
     };
   }
 
@@ -21,6 +22,12 @@ class ColorSchemeGenerator extends Component {
     this.setState({ lockedColors });
   }
 
+  toggleColorList() {
+    this.setState((prevState) => ({
+      showColorList: !prevState.showColorList,
+    }));
+  }
+
   regenerateColor(index) {
     if (!this.state.lockedColors[index]) {
       const { colors } = this.state;
@@ -29,7 +36,22 @@ class ColorSchemeGenerator extends Component {
     }
   }
 
-  copyColorToClipboard(color) {
+  copyAllColorsToClipboard() {
+    const { colors } = this.state;
+    const allColorsText = colors
+      .map((color, index) => `Color ${index + 1}: ${color}`)
+      .join('\n');
+    navigator.clipboard.writeText(allColorsText).then(function() {
+      alert('Copied all colors');
+    }).catch(function() {
+      alert('Copy failed');
+    });
+  }
+
+  // Update copyColorToClipboard to accept the index
+  copyColorToClipboard(index) {
+    const { colors } = this.state;
+    const color = colors[index];
     navigator.clipboard.writeText(color).then(function() {
       alert(`Copied: ${color}`);
     }).catch(function() {
@@ -38,39 +60,92 @@ class ColorSchemeGenerator extends Component {
   }
 
   render() {
-    const { colors, lockedColors } = this.state;
+    const { colors, lockedColors, showColorList } = this.state;
+    const jumbotronStyle = {
+      height: '350px',
+      backgroundImage: `url(./bg-image-20.jpg)`, // Set the background image
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      marginBottom: '30px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    };
     return (
-      <div className="container">
-        <div className="jumbotron text-center" style={{ height: '250px', backgroundColor: 'green', marginBottom: '50px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-          <h1>Random Color Scheme Generator</h1>
+      <>
+        <div className="jumbotron text-center" style={jumbotronStyle}>
+          <h1 style={{ color: 'white' }}>Random Color Scheme Generator</h1>
         </div>
-        <div className="row">
-          {colors.map((color, index) => (
-            <div key={index} className="col-md-4">
-              {/* <div className="color-code">{color}</div> */}
-              <div
-                className="color-box"
-                style={{ backgroundColor: color, height: '400px',borderRadius:'10px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginBottom: '30px' }}
-              >
-                <button
-                className="btn btn-secondary"
-                onClick={() => this.regenerateColor(index)}
+        <div className="container">
+          <div className="color-list" style={{ paddingBottom: '30px' }}>
+            <button
+              className="btn btn-primary"
+              style={{ marginBottom: '20px' }}
+              onClick={() => this.toggleColorList()} // Add click event to toggle color list
+            >
+              {showColorList ? 'Hide Color List' : 'Show Color List'}
+            </button>
+            {showColorList && ( // Render color list only if showColorList is true
+              <ul className="list-group">
+                {colors.map((color, index) => (
+                  <li
+                    className="list-group-item color-item"
+                    key={index}
+                    onClick={() => this.copyColorToClipboard(index)} // Add click event to copy hex
+                  >
+                    <div>
+                      Color {index + 1}: {color}
+                    </div>
+                  </li>
+                ))}
+                <li className="list-group-item">
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => this.copyAllColorsToClipboard()}
+                  >
+                    Copy All Colors
+                  </button>
+                </li>
+              </ul>
+            )}
+          </div>
+          <div className="row">
+            {colors.map((color, index) => (
+              <div key={index} className="col-md-4">
+                <div
+                  className="color-box"
+                  style={{
+                    backgroundColor: color,
+                    height: '400px',
+                    borderRadius: '10px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginBottom: '30px',
+                  }}
                 >
-                Regenerate
-                </button>
-                <br/>
-                <button
-                className="btn btn-primary"
-                onClick={() => this.copyColorToClipboard(color)}
-                >
-                Copy Hex: {color}
-                </button>
+                  <button
+                    className="btn btn-secondary"
+                    onClick={() => this.regenerateColor(index)}
+                  >
+                    Regenerate
+                  </button>
+                  <br />
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => this.copyColorToClipboard(index)}
+                  >
+                    Copy Hex: {color}
+                  </button>
+                </div>
+                {lockedColors[index] && <div className="locked-indicator">Locked</div>}
               </div>
-              {lockedColors[index] && <div className="locked-indicator">Locked</div>}
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
